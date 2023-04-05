2b70116e524039256ac423993c216b2100f63374
efe <eefee1987@gmail.com>
2023-04-05 08:24:40 +0200

    mejores y peores terminado

--- mejorYPeorEquipos.js
+++ mejorYPeorEquipos.js
@@ -7,6 +7,10 @@ El resultado que tiene que aparecer en la consola al ejecutarlo debe ser exactam
 -> El peor equipo es Ciervos Celestes con un total de 10 puntos
  */
 
+'use strict';
+
+// puntuaciones primera ronda
+
 const firstRound = [
   { team: 'Toros Negros', scores: [1, 3, 4, 2, 10, 8] },
   { team: 'Águilas Plateadas', scores: [5, 8, 3, 2, 5, 3] },
@@ -17,40 +21,35 @@ const firstRound = [
   { team: 'Pavos Reales Coral', scores: [2, 3, 2, 1, 4, 3] },
   { team: 'Orcas Moradas', scores: [2, 3, 3, 4] },
 ];
-let puntuaciones = [];
 
-for (const team of firstRound) {
-  const sumScore = team.scores.reduce((acc, number) => {
-    return acc + number;
-  }, 0);
-  puntuaciones.push(Number(sumScore));
-}
+// Creo un array nuevo a partir del original, con el nombre de los equipos
+// y el total de su puntuacion
 
-let mejorPuntuacion = Math.max(...puntuaciones);
-let peorPuntuacion = Math.min(...puntuaciones);
+const totalPoints = firstRound.map((equipo) => {
+  // Retornamos un objeto (un equipo) con su nombre y el total de puntos
+  return {
+    team: equipo.team,
+    totalScore: equipo.scores.reduce((acc, score) => {
+      // Devolver el total de puntos
+      return acc + score;
+    }, 0),
+  };
+});
 
-for (const equipo of firstRound) {
-  const puntosTotales = equipo.scores.reduce((acc, score) => {
-    return acc + score;
-  }, 0);
+// Sacar el mejor y peor equipo
+// Ordeno el array
+totalPoints.sort((a, b) => a.totalScore - b.totalScore);
 
-  if (mejorPuntuacion === puntosTotales) {
-    console.log(
-      'El mejor equipo es ' +
-        equipo.team +
-        ' con un total de ' +
-        puntosTotales +
-        ' puntos'
-    );
-  }
+// El último elemento del array es el primer clasificado
+console.log(
+  `El mejor equipo es ${
+    totalPoints[totalPoints.length - 1].team
+  } con un total de ${totalPoints[totalPoints.length - 1].totalScore} puntos`
+);
 
-  if (peorPuntuacion === puntosTotales) {
-    console.log(
-      'El peor equipo es ' +
-        equipo.team +
-        ' con un total de ' +
-        puntosTotales +
-        ' puntos'
-    );
-  }
-}
+// Ahora que tenemos el array ordenado sabemos que el primer elemento es el último clasificado
+console.log(
+  `El peor equipo es ${totalPoints[0].team} con un total de ${totalPoints[0].totalScore} puntos`
+);
+
+console.log(totalPoints);
